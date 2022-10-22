92b95ced5b657f920539fe80b41e449bba3f7a09
olega211 <106370448+olega211@users.noreply.github.com>
2022-10-23 04:34:45 +0700

    region edit

--- bot.py
+++ bot.py
@@ -195,14 +195,14 @@ async def process_city(message: types.Message, state: FSMContext):
         with app.app_context():
             # get chat
             try:
-                chat = Chats.query.filter_by(region=data['region']).first()
+                chat = Chats.query.filter_by(region=data['region']).first().link
             except Exception as e:
                 chat = 'Данных нет'
 
         # links buttons
         links.add(types.InlineKeyboardButton('Бот для бродажи', url=config.BUY_LINK))
         links.add(types.InlineKeyboardButton('Бот для покупки', url=config.SALE_LINK))
-        links.add(types.InlineKeyboardButton('Ваш чат', url=chat.link))
+        links.add(types.InlineKeyboardButton('Ваш чат', url=chat))
 
         
         try:
